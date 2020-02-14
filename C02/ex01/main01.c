44acdf05cab611734157333d8dbfac27427a4294
cboville2 <60619493+cboville2@users.noreply.github.com>
2020-02-14 14:21:07 +0100

    Add files via upload

---
+++ C02/ex01/main01.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cboville <cboville@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/14 13:46:19 by cboville          #+#    #+#             */
+/*   Updated: 2020/02/14 14:18:18 by cboville         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+
+int	main()
+{
+	char dest[] = "ABCDE";
+	char src[] = "123";
+	int n = 3;
+
+	printf("%s", ft_strncpy(dest, src, n));
+}
