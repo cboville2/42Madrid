5aa81d6f48d396ce79d95f8768c740bfca7e3f88
cboville2 <60619493+cboville2@users.noreply.github.com>
2020-02-25 19:31:08 +0100

    Add files via upload

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cboville <cboville@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/21 11:08:43 by cboville          #+#    #+#             */
+/*   Updated: 2020/02/23 20:53:11 by cboville         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (ft_is_prime(nb) != 1)
+	{
+		nb++;
+	}
+	return (nb);
+}
